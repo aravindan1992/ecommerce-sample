827a59501983dc8d40e0bef02ce4fbd15ab5bb40
Aravindan K <aravindan1992@gmail.com>
2026-02-03 15:09:05 +0530

    user search service created

--- src/main/java/com/example/userservice/exception/UserNotFoundException.java
+++ src/main/java/com/example/userservice/exception/UserNotFoundException.java
@@ -1,11 +1,29 @@
 package com.example.userservice.exception;
 
+/**
+ * Exception thrown when a user is not found.
+ * 
+ * @author Automation Engineer
+ * @version 1.0.0
+ * @since 2024-01-01
+ */
 public class UserNotFoundException extends RuntimeException {
-    
-    public UserNotFoundException(String email) {
-        super(String.format("User not found with email: %s", email));
+
+    /**
+     * Constructs a new UserNotFoundException with the specified detail message.
+     * 
+     * @param message the detail message
+     */
+    public UserNotFoundException(String message) {
+        super(message);
     }
-    
+
+    /**
+     * Constructs a new UserNotFoundException with the specified detail message and cause.
+     * 
+     * @param message the detail message
+     * @param cause the cause
+     */
     public UserNotFoundException(String message, Throwable cause) {
         super(message, cause);
     }
